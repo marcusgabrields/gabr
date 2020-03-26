dc448bb5ef4c71ca8298cd639a50fe269ef3db45
Marcus Gabriel <marcusgabriel.ds@gmail.com>
2020-03-26 19:33:08 -0300

    Adiciona sessão conteudo

---
+++ frontend/js/apps/blog/Content/index.js
@@ -0,0 +1,243 @@
+import React from 'react';
+
+import './styles.scss';
+
+const Content = () => (
+  <>
+    <div className='blog-articles-container'>
+
+      <article className='blog-card'>
+        <header className='header'>
+          <div className='header-image'></div>
+        </header>
+        <div className='body'>
+          <div className='title'>
+            Uma lista abrangente (e honesta) de clichês de UX
+          </div>
+          <div className='sub-title'>
+            Para quem está chegando agora na área.
+          </div>
+          <div className='avatar'>
+            <div className='image'></div>
+            <div className='info-container'>
+              <span className='name'>Fabricio Teixeira</span>
+              <span className='description'>
+                Mar 4, 2019 . 5 min read
+              </span>
+            </div>
+          </div>
+        </div>
+      </article>
+
+      <article className='blog-card'>
+        <header className='header'>
+          <div className='header-image'></div>
+        </header>
+        <div className='body'>
+          <div className='title'>
+            Uma lista abrangente (e honesta) de clichês de UX
+          </div>
+          <div className='sub-title'>
+            Para quem está chegando agora na área.
+          </div>
+          <div className='avatar'>
+            <div className='image'></div>
+            <div className='info-container'>
+              <span className='name'>Fabricio Teixeira</span>
+              <span className='description'>
+                Mar 4, 2019 . 5 min read
+              </span>
+            </div>
+          </div>
+        </div>
+      </article>
+
+      <article className='blog-card'>
+        <header className='header'>
+          <div className='header-image'></div>
+        </header>
+        <div className='body'>
+          <div className='title'>
+            Uma lista abrangente (e honesta) de clichês de UX
+          </div>
+          <div className='sub-title'>
+            Para quem está chegando agora na área.
+          </div>
+          <div className='avatar'>
+            <div className='image'></div>
+            <div className='info-container'>
+              <span className='name'>Fabricio Teixeira</span>
+              <span className='description'>
+                Mar 4, 2019 . 5 min read
+              </span>
+            </div>
+          </div>
+        </div>
+      </article>
+
+      <article className='blog-card'>
+        <header className='header'>
+          <div className='header-image'></div>
+        </header>
+        <div className='body'>
+          <div className='title'>
+            Uma lista abrangente (e honesta) de clichês de UX
+          </div>
+          <div className='sub-title'>
+            Para quem está chegando agora na área.
+          </div>
+          <div className='avatar'>
+            <div className='image'></div>
+            <div className='info-container'>
+              <span className='name'>Fabricio Teixeira</span>
+              <span className='description'>
+                Mar 4, 2019 . 5 min read
+              </span>
+            </div>
+          </div>
+        </div>
+      </article>
+
+      <article className='blog-card'>
+        <header className='header'>
+          <div className='header-image'></div>
+        </header>
+        <div className='body'>
+          <div className='title'>
+            Uma lista abrangente (e honesta) de clichês de UX
+          </div>
+          <div className='sub-title'>
+            Para quem está chegando agora na área.
+          </div>
+          <div className='avatar'>
+            <div className='image'></div>
+            <div className='info-container'>
+              <span className='name'>Fabricio Teixeira</span>
+              <span className='description'>
+                Mar 4, 2019 . 5 min read
+              </span>
+            </div>
+          </div>
+        </div>
+      </article>
+
+      <article className='blog-card'>
+        <header className='header'>
+          <div className='header-image'></div>
+        </header>
+        <div className='body'>
+          <div className='title'>
+            Uma lista abrangente (e honesta) de clichês de UX
+          </div>
+          <div className='sub-title'>
+            Para quem está chegando agora na área.
+          </div>
+          <div className='avatar'>
+            <div className='image'></div>
+            <div className='info-container'>
+              <span className='name'>Fabricio Teixeira</span>
+              <span className='description'>
+                Mar 4, 2019 . 5 min read
+              </span>
+            </div>
+          </div>
+        </div>
+      </article>
+
+      <article className='blog-card'>
+        <header className='header'>
+          <div className='header-image'></div>
+        </header>
+        <div className='body'>
+          <div className='title'>
+            Uma lista abrangente (e honesta) de clichês de UX
+          </div>
+          <div className='sub-title'>
+            Para quem está chegando agora na área.
+          </div>
+          <div className='avatar'>
+            <div className='image'></div>
+            <div className='info-container'>
+              <span className='name'>Fabricio Teixeira</span>
+              <span className='description'>
+                Mar 4, 2019 . 5 min read
+              </span>
+            </div>
+          </div>
+        </div>
+      </article>
+
+      <article className='blog-card'>
+        <header className='header'>
+          <div className='header-image'></div>
+        </header>
+        <div className='body'>
+          <div className='title'>
+            Uma lista abrangente (e honesta) de clichês de UX
+          </div>
+          <div className='sub-title'>
+            Para quem está chegando agora na área.
+          </div>
+          <div className='avatar'>
+            <div className='image'></div>
+            <div className='info-container'>
+              <span className='name'>Fabricio Teixeira</span>
+              <span className='description'>
+                Mar 4, 2019 . 5 min read
+              </span>
+            </div>
+          </div>
+        </div>
+      </article>
+
+      <article className='blog-card'>
+        <header className='header'>
+          <div className='header-image'></div>
+        </header>
+        <div className='body'>
+          <div className='title'>
+            Uma lista abrangente (e honesta) de clichês de UX
+          </div>
+          <div className='sub-title'>
+            Para quem está chegando agora na área.
+          </div>
+          <div className='avatar'>
+            <div className='image'></div>
+            <div className='info-container'>
+              <span className='name'>Fabricio Teixeira</span>
+              <span className='description'>
+                Mar 4, 2019 . 5 min read
+              </span>
+            </div>
+          </div>
+        </div>
+      </article>
+
+      <article className='blog-card'>
+        <header className='header'>
+          <div className='header-image'></div>
+        </header>
+        <div className='body'>
+          <div className='title'>
+            Uma lista abrangente (e honesta) de clichês de UX
+          </div>
+          <div className='sub-title'>
+            Para quem está chegando agora na área.
+          </div>
+          <div className='avatar'>
+            <div className='image'></div>
+            <div className='info-container'>
+              <span className='name'>Fabricio Teixeira</span>
+              <span className='description'>
+                Mar 4, 2019 . 5 min read
+              </span>
+            </div>
+          </div>
+        </div>
+      </article>
+
+    </div>
+  </>
+)
+
+export default Content
