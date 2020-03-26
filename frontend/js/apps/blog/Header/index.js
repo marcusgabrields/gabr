b8effe3d37ddb61c4ee1e52a9a3ae171d9258adf
Marcus Gabriel <marcusgabriel.ds@gmail.com>
2020-03-25 22:11:06 -0300

    Adiciona main content no header

--- frontend/js/apps/blog/Header/index.js
+++ frontend/js/apps/blog/Header/index.js
@@ -6,12 +6,19 @@ import './styles.scss';
 
 const Header = () => (
   <section className='blog-top-section'>
+    <div className='main-content'>
+      <h2>gab<span>r</span></h2>
+      <p>
+        Um lugar para acolher os resultados das minhas catarses.
+      </p>
+    </div>
     <div className='sticky-bottom'>
       <div className='links'>
         <div className='links-container'>
           <a className='link' href='#'>EM DESTAQUE</a>
-          <a className='link' href='#'>EM DESTAQUE</a>
-          <a className='link' href='#'>EM DESTAQUE</a>
+          <a className='link' href='#'>PYTHON</a>
+          <a className='link' href='#'>DJANGO</a>
+          <a className='link' href='#'>REST FRAMEWORK</a>
         </div>
       </div>
       <div className='social'>
