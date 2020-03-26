88a47a8d7a0ea0c137ccf1b0838d46b8ec06ccf8
Marcus Gabriel <marcusgabriel.ds@gmail.com>
2020-03-26 19:32:53 -0300

    Adiciona sessão do contudo na página do blog

--- frontend/js/apps/blog/Blog/index.js
+++ frontend/js/apps/blog/Blog/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
 import Header from '../Header';
+import Content from '../Content';
+
+import './styles.scss';
 
 const Blog = () => (
   <>
-    <Header />
+    <section className='blog-top-wrapper'>
+      <Header />
+    </section>
+    <section className='blog-content-wrapper'>
+      <Content />
+    </section>
   </>
 )
 
