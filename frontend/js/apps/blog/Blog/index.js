80c3176422b0786c580ac1ecb63a03bddbf175bf
Marcus Gabriel <marcusgabriel.ds@gmail.com>
2020-03-30 21:07:38 -0300

    Ajusta página de blog para que ela populer o conteudo a partir de um endpoint

--- frontend/js/apps/blog/Blog/index.js
+++ frontend/js/apps/blog/Blog/index.js
@@ -3,17 +3,41 @@ import React from 'react';
 import Header from '../Header';
 import Content from '../Content';
 
+import { ArticleService } from '../../../services';
+
 import './styles.scss';
 
-const Blog = () => (
-  <>
-    <section className='blog-top-wrapper'>
-      <Header />
-    </section>
-    <section className='blog-content-wrapper'>
-      <Content />
-    </section>
-  </>
-)
+class Blog extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      articles: [],
+      pageLoading: true,
+    }
+  }
 
-export default Blog
+  async componentDidMount() {
+    const { data } = await ArticleService.list()
+    this.setState({ articles: data, pageLoading: false })
+  }
+
+  render() {
+    const { articles, pageLoading } = this.state;
+
+    return (
+      <>
+        <section className='blog-top-wrapper'>
+          <Header />
+        </section>
+        <section className='blog-content-wrapper'>
+          <Content
+            articles={ articles }
+            pageLoading={ pageLoading }
+          />
+        </section>
+      </>
+    )
+  }
+}
+
+export default Blog;
