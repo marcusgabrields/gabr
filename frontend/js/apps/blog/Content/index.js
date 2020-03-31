217c4a98866287ff9440a6898354042cc1c26b3f
Marcus Gabriel <marcusgabriel.ds@gmail.com>
2020-03-30 21:08:01 -0300

    Ajusta content para exibir conteudo retornado do endpoint

--- frontend/js/apps/blog/Content/index.js
+++ frontend/js/apps/blog/Content/index.js
@@ -1,243 +1,61 @@
 import React from 'react';
 
+import Loader from 'react-loaders'
+
 import './styles.scss';
 
-const Content = () => (
-  <>
-    <div className='blog-articles-container'>
 
-      <article className='blog-card'>
-        <header className='header'>
-          <div className='header-image'></div>
-        </header>
-        <div className='body'>
-          <div className='title'>
-            Uma lista abrangente (e honesta) de clichês de UX
-          </div>
-          <div className='sub-title'>
-            Para quem está chegando agora na área.
-          </div>
-          <div className='avatar'>
-            <div className='image'></div>
-            <div className='info-container'>
-              <span className='name'>Fabricio Teixeira</span>
-              <span className='description'>
-                Mar 4, 2019 . 5 min read
-              </span>
+class Content extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    const { articles, pageLoading } = this.props
+
+    return (
+      <>
+        {
+          pageLoading && (
+            <div className='loader-wrapper'>
+              <div class="lds-ripple"><div></div><div></div></div>
             </div>
-          </div>
-        </div>
-      </article>
+          )
+        }
 
-      <article className='blog-card'>
-        <header className='header'>
-          <div className='header-image'></div>
-        </header>
-        <div className='body'>
-          <div className='title'>
-            Uma lista abrangente (e honesta) de clichês de UX
-          </div>
-          <div className='sub-title'>
-            Para quem está chegando agora na área.
-          </div>
-          <div className='avatar'>
-            <div className='image'></div>
-            <div className='info-container'>
-              <span className='name'>Fabricio Teixeira</span>
-              <span className='description'>
-                Mar 4, 2019 . 5 min read
-              </span>
-            </div>
-          </div>
+        <div className='blog-articles-container'>
+          {
+            Array.isArray(articles) && articles.map((article) => (
+              <article className='blog-card'>
+                <header className='header'>
+                  <div className='header-image'></div>
+                </header>
+                <div className='body'>
+                  <div className='title'>
+                    { article.title }
+                  </div>
+                  <div className='sub-title'>
+                    { article.subtitle }
+                  </div>
+                  <div className='avatar'>
+                    <div className='image'></div>
+                    <div className='info-container'>
+                      <span className='name'>
+                        { article.author === null ? 'Anônimo' : article.author.name }
+                      </span>
+                      <span className='description'>
+                        { article.publish_date } . { article.reading_time } min read
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </article>
+            ))
+          }
         </div>
-      </article>
-
-      <article className='blog-card'>
-        <header className='header'>
-          <div className='header-image'></div>
-        </header>
-        <div className='body'>
-          <div className='title'>
-            Uma lista abrangente (e honesta) de clichês de UX
-          </div>
-          <div className='sub-title'>
-            Para quem está chegando agora na área.
-          </div>
-          <div className='avatar'>
-            <div className='image'></div>
-            <div className='info-container'>
-              <span className='name'>Fabricio Teixeira</span>
-              <span className='description'>
-                Mar 4, 2019 . 5 min read
-              </span>
-            </div>
-          </div>
-        </div>
-      </article>
-
-      <article className='blog-card'>
-        <header className='header'>
-          <div className='header-image'></div>
-        </header>
-        <div className='body'>
-          <div className='title'>
-            Uma lista abrangente (e honesta) de clichês de UX
-          </div>
-          <div className='sub-title'>
-            Para quem está chegando agora na área.
-          </div>
-          <div className='avatar'>
-            <div className='image'></div>
-            <div className='info-container'>
-              <span className='name'>Fabricio Teixeira</span>
-              <span className='description'>
-                Mar 4, 2019 . 5 min read
-              </span>
-            </div>
-          </div>
-        </div>
-      </article>
-
-      <article className='blog-card'>
-        <header className='header'>
-          <div className='header-image'></div>
-        </header>
-        <div className='body'>
-          <div className='title'>
-            Uma lista abrangente (e honesta) de clichês de UX
-          </div>
-          <div className='sub-title'>
-            Para quem está chegando agora na área.
-          </div>
-          <div className='avatar'>
-            <div className='image'></div>
-            <div className='info-container'>
-              <span className='name'>Fabricio Teixeira</span>
-              <span className='description'>
-                Mar 4, 2019 . 5 min read
-              </span>
-            </div>
-          </div>
-        </div>
-      </article>
-
-      <article className='blog-card'>
-        <header className='header'>
-          <div className='header-image'></div>
-        </header>
-        <div className='body'>
-          <div className='title'>
-            Uma lista abrangente (e honesta) de clichês de UX
-          </div>
-          <div className='sub-title'>
-            Para quem está chegando agora na área.
-          </div>
-          <div className='avatar'>
-            <div className='image'></div>
-            <div className='info-container'>
-              <span className='name'>Fabricio Teixeira</span>
-              <span className='description'>
-                Mar 4, 2019 . 5 min read
-              </span>
-            </div>
-          </div>
-        </div>
-      </article>
-
-      <article className='blog-card'>
-        <header className='header'>
-          <div className='header-image'></div>
-        </header>
-        <div className='body'>
-          <div className='title'>
-            Uma lista abrangente (e honesta) de clichês de UX
-          </div>
-          <div className='sub-title'>
-            Para quem está chegando agora na área.
-          </div>
-          <div className='avatar'>
-            <div className='image'></div>
-            <div className='info-container'>
-              <span className='name'>Fabricio Teixeira</span>
-              <span className='description'>
-                Mar 4, 2019 . 5 min read
-              </span>
-            </div>
-          </div>
-        </div>
-      </article>
-
-      <article className='blog-card'>
-        <header className='header'>
-          <div className='header-image'></div>
-        </header>
-        <div className='body'>
-          <div className='title'>
-            Uma lista abrangente (e honesta) de clichês de UX
-          </div>
-          <div className='sub-title'>
-            Para quem está chegando agora na área.
-          </div>
-          <div className='avatar'>
-            <div className='image'></div>
-            <div className='info-container'>
-              <span className='name'>Fabricio Teixeira</span>
-              <span className='description'>
-                Mar 4, 2019 . 5 min read
-              </span>
-            </div>
-          </div>
-        </div>
-      </article>
-
-      <article className='blog-card'>
-        <header className='header'>
-          <div className='header-image'></div>
-        </header>
-        <div className='body'>
-          <div className='title'>
-            Uma lista abrangente (e honesta) de clichês de UX
-          </div>
-          <div className='sub-title'>
-            Para quem está chegando agora na área.
-          </div>
-          <div className='avatar'>
-            <div className='image'></div>
-            <div className='info-container'>
-              <span className='name'>Fabricio Teixeira</span>
-              <span className='description'>
-                Mar 4, 2019 . 5 min read
-              </span>
-            </div>
-          </div>
-        </div>
-      </article>
-
-      <article className='blog-card'>
-        <header className='header'>
-          <div className='header-image'></div>
-        </header>
-        <div className='body'>
-          <div className='title'>
-            Uma lista abrangente (e honesta) de clichês de UX
-          </div>
-          <div className='sub-title'>
-            Para quem está chegando agora na área.
-          </div>
-          <div className='avatar'>
-            <div className='image'></div>
-            <div className='info-container'>
-              <span className='name'>Fabricio Teixeira</span>
-              <span className='description'>
-                Mar 4, 2019 . 5 min read
-              </span>
-            </div>
-          </div>
-        </div>
-      </article>
-
-    </div>
-  </>
-)
+      </>
+    )
+  }
+}
 
 export default Content
