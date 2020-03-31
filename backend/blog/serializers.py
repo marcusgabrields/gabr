397c7e4c98777c29d9ab8bb7892cb17ee6772637
Marcus Gabriel <marcusgabriel.ds@gmail.com>
2020-03-30 21:05:07 -0300

    Ajusta serializer para renderizer somente os campor necessarios

--- backend/blog/serializers.py
+++ backend/blog/serializers.py
@@ -1,12 +1,16 @@
-from codecs import lookup
 from rest_framework import serializers
 
+from perfil.serializers import PerfilSerializer
 from .models import Article
 
 
 class ArticleSerializer(serializers.ModelSerializer):
 
+    author = PerfilSerializer()
+    publish_date = serializers.DateField(format='%b %d, %Y')
+
     class Meta:
         model = Article
-        fields = '__all__'
         lookup_field = 'slug'
+        fields = ['slug', 'title', 'subtitle', 'publish_date', 'reading_time',
+                  'author']
