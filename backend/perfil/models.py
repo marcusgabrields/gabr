8d29d5786eb1d01e27cbf99b27d6d1134aa52f26
Marcus Gabriel <marcusgabriel.ds@gmail.com>
2020-03-30 21:06:12 -0300

    Adiciona modelo para o Perfil

---
+++ backend/perfil/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from common.models import TimeStampedModel
+
+
+class Perfil(TimeStampedModel):
+
+    user = models.OneToOneField(
+        'users.User',
+        on_delete=models.CASCADE,
+        primary_key=True,
+        verbose_name=_('user'),
+    )
+    name = models.CharField(_('name'), max_length=255)
+
+    def __str__(self):
+        return self.name
