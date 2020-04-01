6308aca189c55cbc4f7f0e22aa76426653da8e78
Marcus Gabriel <marcusgabriel.ds@gmail.com>
2020-03-31 22:21:10 -0300

    Adiciona model Category no Django Admin

--- backend/blog/admin.py
+++ backend/blog/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
-from .models import Article
+from .models import Article, Category
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
 
     prepopulated_fields = {'slug': ('title', )}
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+
+    prepopulated_fields = {'slug': ('name', )}
