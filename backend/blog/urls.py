cdc9299b8bdbcb54acf20fbb3dc1d4d0d2102f04
Marcus Gabriel <marcusgabriel.ds@gmail.com>
2020-03-31 22:21:52 -0300

    Adiciona endpoint para Category

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -2,12 +2,12 @@ from django.urls import include, path
 
 from rest_framework import routers
 
-from .views import ArticleViewSet
+from .views import ArticleViewSet, CategoryViewSet
 
 
 router = routers.DefaultRouter()
 router.register('articles', ArticleViewSet)
-
+router.register('categories', CategoryViewSet)
 
 urlpatterns = [
     path('', include(router.urls))
--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,8 +1,10 @@
 from rest_framework import viewsets
 from rest_framework import permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-from .serializers import ArticleSerializer
-from .models import Article
+from .serializers import ArticleSerializer, CategorySerializer
+from .models import Article, Category
 
 
 class ArticleViewSet(viewsets.ModelViewSet):
@@ -19,3 +21,26 @@ class ArticleViewSet(viewsets.ModelViewSet):
             permission_classes = [permissions.IsAuthenticated]
 
         return [permission() for permission in permission_classes]
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all().order_by('priority')
+    lookup_field = 'slug'
+
+    def get_permissions(self):
+        if self.action in ['list', 'retrive']:
+            permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+        else:
+            permission_classes = [permissions.IsAuthenticated]
+
+        return [permission() for permission in permission_classes]
+
+    def retrieve(self, request, slug=None):
+
+        articles = Article.objects.filter(draft=False, categories__slug__in=[slug])
+        serializer = ArticleSerializer(articles, many=True)
+
+        return Response(serializer.data)
